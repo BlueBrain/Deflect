7f0376d7284334f50260b546bd600ae37a1959ea
Raphael Dumusc <raphael.dumusc@epfl.ch>
2015-06-05 16:14:59 +0200

    Using common_gui_application() for DesktopStreamer

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -6,16 +6,8 @@
 # Info: http://www.itk.org/Wiki/CMake:Component_Install_With_CPack
 
 set(CPACK_PACKAGE_EXECUTABLES "${DESKTOPSTREAMER_APP_NAME}")
-set(CPACK_PACKAGE_NAME "${DESKTOPSTREAMER_APP_NAME}")
 set(CPACK_PACKAGE_CONTACT "Daniel Nachbaur <daniel.nachbaur@epfl.ch>")
 set(CPACK_PACKAGE_VENDOR "https://github.com/BlueBrain/Deflect")
-
-set(CPACK_COMPONENT_DESKTOPSTREAMER_DISPLAY_NAME "DesktopStreamer Application")
-set(CPACK_COMPONENT_DESKTOPSTREAMER_DESCRIPTION "DesktopStreamer is an application that lets you stream your desktop to a running DisplayCluster instance.")
-set(CPACK_COMPONENT_DESKTOPSTREAMER_DEPENDS Deflect)
-
-# Currently we only package desktopstreamer and the Deflect library
-set(CPACK_COMPONENTS_ALL desktopstreamer lib)
 
 # Linux Debian specific settings
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "qtbase5-dev, libturbojpeg" )
