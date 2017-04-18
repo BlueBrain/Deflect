4542abe17b0d48040e251325e7f0517708d65fed
Raphael Dumusc <raphael.dumusc@epfl.ch>
2017-04-18 14:10:12 +0200

    DesktopStreamer OSX: read default hosts from file

---
+++ apps/DesktopStreamer/defaults.cpp
@@ -0,0 +1,87 @@
+/*********************************************************************/
+/* Copyright (c) 2017, EPFL/Blue Brain Project                       */
+/*                     Raphael Dumusc <raphael.dumusc@epfl.ch>       */
+/* All rights reserved.                                              */
+/*                                                                   */
+/* Redistribution and use in source and binary forms, with or        */
+/* without modification, are permitted provided that the following   */
+/* conditions are met:                                               */
+/*                                                                   */
+/*   1. Redistributions of source code must retain the above         */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer.                                                  */
+/*                                                                   */
+/*   2. Redistributions in binary form must reproduce the above      */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer in the documentation and/or other materials       */
+/*      provided with the distribution.                              */
+/*                                                                   */
+/*    THIS  SOFTWARE IS PROVIDED  BY THE  UNIVERSITY OF  TEXAS AT    */
+/*    AUSTIN  ``AS IS''  AND ANY  EXPRESS OR  IMPLIED WARRANTIES,    */
+/*    INCLUDING, BUT  NOT LIMITED  TO, THE IMPLIED  WARRANTIES OF    */
+/*    MERCHANTABILITY  AND FITNESS FOR  A PARTICULAR  PURPOSE ARE    */
+/*    DISCLAIMED.  IN  NO EVENT SHALL THE UNIVERSITY  OF TEXAS AT    */
+/*    AUSTIN OR CONTRIBUTORS BE  LIABLE FOR ANY DIRECT, INDIRECT,    */
+/*    INCIDENTAL,  SPECIAL, EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES    */
+/*    (INCLUDING, BUT  NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE    */
+/*    GOODS  OR  SERVICES; LOSS  OF  USE,  DATA,  OR PROFITS;  OR    */
+/*    BUSINESS INTERRUPTION) HOWEVER CAUSED  AND ON ANY THEORY OF    */
+/*    LIABILITY, WHETHER  IN CONTRACT, STRICT  LIABILITY, OR TORT    */
+/*    (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT    */
+/*    OF  THE  USE OF  THIS  SOFTWARE,  EVEN  IF ADVISED  OF  THE    */
+/*    POSSIBILITY OF SUCH DAMAGE.                                    */
+/*                                                                   */
+/* The views and conclusions contained in the software and           */
+/* documentation are those of the authors and should not be          */
+/* interpreted as representing official policies, either expressed   */
+/* or implied, of The University of Texas at Austin.                 */
+/*********************************************************************/
+
+#include "defaults.h"
+
+#include <QApplication>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+
+#include <iostream>
+
+namespace defaults
+{
+std::vector<std::pair<QString, QString>> getHosts()
+{
+#ifdef __APPLE__
+    const auto appPath = QApplication::applicationDirPath();
+    QFile file{appPath + "/../Resources/defaults.json"};
+    if (!file.exists() || !file.open(QIODevice::ReadOnly))
+    {
+        std::cerr << "could not open file 'defaults.json'" << std::endl;
+        return {};
+    }
+    QJsonParseError error;
+    const auto doc = QJsonDocument::fromJson(file.readAll(), &error);
+    if (doc.isNull())
+    {
+        std::cerr << "defaults.json parsing error: "
+                  << error.errorString().toStdString() << std::endl;
+        return {};
+    }
+    std::vector<std::pair<QString, QString>> hosts;
+    for (const auto& value : doc.object()["hosts"].toArray())
+    {
+        const auto entry = value.toObject();
+        if (entry.size() != 1)
+            continue;
+        const auto name = entry.begin().key();
+        const auto addr = entry.begin().value().toString();
+        if (!name.isEmpty() && !addr.isEmpty())
+            hosts.emplace_back(name, addr);
+    }
+    return hosts;
+#else
+    return {_DEFAULT_HOSTS}; // CMake DEFLECT_DESKTOPSTREAMER_HOSTS
+#endif
+}
+}
